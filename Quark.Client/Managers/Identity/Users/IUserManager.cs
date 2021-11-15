35ad862ac7bfe5aa0b234570221f2396ed635f7b
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-15 21:32:16 +0530

    Add Httpclient manager for User API controller endpoints

---
+++ Quark.Client/Managers/Identity/Users/IUserManager.cs
@@ -0,0 +1,26 @@
+﻿using Quark.Core.Requests.Identity;
+using Quark.Core.Responses.Identity;
+using Quark.Shared.Wrapper;
+
+namespace Quark.Client.Managers.Identity.Users;
+
+public interface IUserManager
+{
+    Task<IResult<List<UserResponse>>> GetAllAsync();
+
+    Task<IResult> ForgotPasswordAsync(ForgotPasswordRequest request);
+
+    Task<IResult> ResetPasswordAsync(ResetPasswordRequest request);
+
+    Task<IResult<UserResponse>> GetAsync(string userId);
+
+    Task<IResult<UserRolesResponse>> GetRolesAsync(string userId);
+
+    Task<IResult> RegisterUserAsync(RegisterRequest request);
+
+    Task<IResult> ToggleUserStatusAsync(ToggleUserStatusRequest request);
+
+    Task<IResult> UpdateRolesAsync(UpdateUserRolesRequest request);
+
+    //Task<string> ExportToExcelAsync(string searchString = "");
+}
