cbe98b733a4904e6780f8929ff06dd48597a9618
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-15 16:32:37 +0530

    Add a Json Validator for string type using Fluent Validation

---
+++ Quark.Core/Validators/JsonValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using Quark.Core.Interfaces.Serialization.Serializers;
+
+namespace Quark.Core.Validators;
+
+public class JsonValidator<T> : PropertyValidator<T, string>
+{
+    private readonly IJsonSerializer _jsonSerializer;
+
+    public JsonValidator(IJsonSerializer jsonSerializer)
+    {
+        _jsonSerializer = jsonSerializer;
+    }
+    public override string Name => "JsonValidator";
+
+    public override bool IsValid(ValidationContext<T> context, string value)
+    {
+        var isJson = true;
+        value = value.Trim();
+        try
+        {
+            _jsonSerializer.Deserialize<object>(value);
+        }
+        catch
+        {
+            isJson = false;
+        }
+        isJson = isJson && value.StartsWith("{") && value.EndsWith("}")
+                 || value.StartsWith("[") && value.EndsWith("]");
+
+        return isJson;
+    }
+
+    protected override string GetDefaultMessageTemplate(string errorCode)
+        => "{PropertyName} must be json string.";
+}
