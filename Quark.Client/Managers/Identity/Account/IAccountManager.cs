d8f72dfec518fe5abe392c680832e6e1bb9290f9
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-15 09:36:27 +0530

    Add HttpClient Mannger for Account API Controller Endpoints

---
+++ Quark.Client/Managers/Identity/Account/IAccountManager.cs
@@ -0,0 +1,12 @@
+﻿using Quark.Core.Requests.Identity;
+using Quark.Shared.Wrapper;
+
+namespace Quark.Client.Managers.Identity.Account;
+
+public interface IAccountManager
+{
+    Task<IResult> ChangePasswordAsync(ChangePasswordRequest model);
+    Task<IResult<string>> GetProfilePictureAsync(string userId);
+    Task<IResult> UpdateProfileAsync(UpdateProfileRequest model);
+    Task<IResult<string>> UpdateProfilePictureAsync(UpdateProfilePictureRequest request, string userId);
+}
