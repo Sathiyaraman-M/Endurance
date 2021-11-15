b73c1679c98fb4b34a870aaf448ea7c655454f8c
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-15 16:28:29 +0530

    Extending ClaimPrincipal class to quickly retrieve claim values

---
+++ Quark.Client/Extensions/ClaimExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+namespace Quark.Client.Extensions;
+
+internal static class ClaimsPrincipalExtensions
+{
+    internal static string GetEmail(this ClaimsPrincipal claimsPrincipal)
+        => claimsPrincipal.FindFirstValue(ClaimTypes.Email);
+
+    internal static string GetUserName(this ClaimsPrincipal claimsPrincipal)
+        => claimsPrincipal.FindFirstValue(ClaimTypes.Name);
+
+    internal static string GetFullName(this ClaimsPrincipal claimsPrincipal)
+        => claimsPrincipal.FindFirstValue("FullName");
+
+    internal static string GetPhoneNumber(this ClaimsPrincipal claimsPrincipal)
+        => claimsPrincipal.FindFirstValue(ClaimTypes.MobilePhone);
+
+    internal static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+       => claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+}
