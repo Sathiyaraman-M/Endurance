16e313dae5e9d81285722f6e7f65d9bf79f53af8
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-19 11:43:56 +0530

    Add IExcelService interface to be used for "Export to Excel" feature

---
+++ Quark.Core/Interfaces/Services/IExcelService.cs
@@ -0,0 +1,6 @@
+﻿namespace Quark.Core.Interfaces.Services;
+
+public interface IExcelService
+{
+    Task<string> ExportAsync<TData>(IEnumerable<TData> data, Dictionary<string, Func<TData, object>> mappings, string sheetName = "Sheet1");
+}
