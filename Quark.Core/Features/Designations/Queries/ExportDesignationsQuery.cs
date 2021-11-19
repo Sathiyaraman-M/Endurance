8b6a76da6fcb1e1e48bf4a9abd3c52a14a65489b
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-19 12:22:17 +0530

    Add "Export to Excel" feature to Designation

---
+++ Quark.Core/Features/Designations/Queries/ExportDesignationsQuery.cs
@@ -0,0 +1,34 @@
+﻿namespace Quark.Core.Features.Designations.Queries;
+
+public class ExportDesignationsQuery : IRequest<Result<string>>
+{
+    public ExportDesignationsQuery(string searchString = "")
+    {
+        SearchString = searchString;
+    }
+
+    public string SearchString { get; }
+}
+
+internal class ExportDesignationsQueryHandler : IRequestHandler<ExportDesignationsQuery, Result<string>>
+{
+    private readonly IUnitOfWork<int> _unitOfWork;
+    private readonly IExcelService _excelService;
+
+    public ExportDesignationsQueryHandler(IUnitOfWork<int> unitOfWork, IExcelService excelService)
+    {
+        _unitOfWork = unitOfWork;
+        _excelService = excelService;
+    }
+    public async Task<Result<string>> Handle(ExportDesignationsQuery request, CancellationToken cancellationToken)
+    {
+        var desigSpec = new DesignationFilterSpecification(request.SearchString);
+        var designations = await _unitOfWork.Repository<Designation>().Entities.Specify(desigSpec).ToListAsync(cancellationToken);
+        var data = await _excelService.ExportAsync(designations, new Dictionary<string, Func<Designation, object>>
+        {
+            { "Id", x => x.Id },
+            { "Name", x=> x.Name }
+        }, "Designations");
+        return await Result<string>.SuccessAsync(data);
+    }
+}
