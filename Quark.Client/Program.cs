617a17d7d6aa0c64914f5638faa294c09690c378
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-22 18:05:22 +0530

    Add global usings to the client project

--- Quark.Client/Program.cs
+++ Quark.Client/Program.cs
@@ -1,5 +1,12 @@
+global using MudBlazor;
+global using Quark.Client.Extensions;
+global using Quark.Core.Responses;
+global using Quark.Shared.Constants;
+global using Quark.Shared.Constants.Permission;
+global using Quark.Shared.Wrapper;
+global using System.Net.Http.Json;
+global using System.Security.Claims;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Quark.Client.Extensions;
 
 var host = WebAssemblyHostBuilder.CreateDefault(args).AddRootComponent().AddClientServices().Build();
 host.Services.GetService<Navigation>();
