8ada2ebf1aba3d7fe4b6289e43e24fc99155d201
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-24 11:46:03 +0530

    Transformed Program.cs to use new minimal API hosting model

--- Quark.Server/Program.cs
+++ Quark.Server/Program.cs
@@ -3,13 +3,59 @@ global using Microsoft.AspNetCore.Mvc;
 global using Quark.Server.Controllers.Utility;
 global using Quark.Shared.Constants;
 global using Quark.Shared.Constants.Permission;
+using FluentValidation.AspNetCore;
+using Hangfire;
+using Microsoft.Extensions.FileProviders;
+using Quark.Core.Configurations;
+using Quark.Core.Extensions;
+using Quark.Infrastructure.Extensions;
+using Quark.Server.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders().AddConsole();
-builder.Host.ConfigureWebHost(webBuilder =>
+builder.Services.AddCors();
+builder.Services.AddCurrentUserService();
+builder.Services.AddSerialization();
+builder.Services.AddDatabase(builder.Configuration);
+builder.Services.AddServerStorage();
+builder.Services.AddIdentity();
+builder.Services.AddJwtAuthentication(builder.Services.GetApplicationSettings(builder.Configuration));
+builder.Services.AddApplicationLayer();
+builder.Services.AddApplicationServices();
+builder.Services.AddRepositories();
+builder.Services.RegisterSwagger();
+builder.Services.AddInfrastructureMappings();
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddHangfireServer();
+builder.Services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<AppConfiguration>());
+builder.Services.AddRazorPages();
+builder.Services.AddLazyCache();
+
+var app = builder.Build();
+
+app.UseCors();
+app.UseExceptionHandling(builder.Environment);
+app.UseHttpsRedirection();
+app.UseMiddleware<ErrorHandlerMiddleware>();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
 {
-    webBuilder.UseStaticWebAssets();
-    webBuilder.UseStartup<Startup>();
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Files")),
+    RequestPath = new PathString("/Files")
 });
 
-await builder.Build().RunAsync();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHangfireDashboard("/jobs", new DashboardOptions
+{
+    DashboardTitle = "Library Management System Jobs",
+    Authorization = new[] { new HangfireAuthorizationFilter() }
+});
+
+app.UseEndpoints();
+app.ConfigureSwagger();
+app.InitializeSeeding();
+
+await app.RunAsync();
