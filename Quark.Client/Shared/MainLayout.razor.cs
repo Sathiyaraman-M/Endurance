9d3fbb10aee28580446c8080478db561fa331c76
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-26 12:26:40 +0530

    Added DarkMode support and Drawer swipe control

--- Quark.Client/Shared/MainLayout.razor.cs
+++ Quark.Client/Shared/MainLayout.razor.cs
@@ -15,6 +15,7 @@ public partial class MainLayout
 
     protected override async Task OnInitializedAsync()
     {
+        _currentTheme = _defaultTheme;
         User = await authStateProvider.GetAuthenticationStateProviderUserAsync();
         if (User == null) return;
         if (User.Identity.IsAuthenticated)
@@ -51,6 +52,65 @@ public partial class MainLayout
             navigationManager.NavigateTo("/");
         }
     }
+
+    private void OnSwipe(SwipeDirection direction)
+    {
+        if (direction == SwipeDirection.LeftToRight && !_drawerOpen)
+        {
+            _drawerOpen = true;
+            StateHasChanged();
+        }
+        else if (direction == SwipeDirection.RightToLeft && _drawerOpen)
+        {
+            _drawerOpen = false;
+            StateHasChanged();
+        }
+    }
+
+    private void DarkMode() => _currentTheme = _currentTheme == _defaultTheme ? _darkTheme : _defaultTheme;
+
+    private MudTheme _currentTheme = new();
+    private readonly MudTheme _defaultTheme =
+        new()
+        {
+            Palette = new Palette()
+            {
+                Black = "#272c34"
+            }
+        };
+    private readonly MudTheme _darkTheme =
+        new()
+        {
+            Palette = new Palette()
+            {
+                Primary = "#776be7",
+                Black = "#27272f",
+                Background = "#32333d",
+                BackgroundGrey = "#27272f",
+                Surface = "#373740",
+                DrawerBackground = "#27272f",
+                DrawerText = "rgba(255,255,255, 0.50)",
+                DrawerIcon = "rgba(255,255,255, 0.50)",
+                AppbarBackground = "#27272f",
+                AppbarText = "rgba(255,255,255, 0.70)",
+                TextPrimary = "rgba(255,255,255, 0.70)",
+                TextSecondary = "rgba(255,255,255, 0.50)",
+                ActionDefault = "#adadb1",
+                ActionDisabled = "rgba(255,255,255, 0.26)",
+                ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+                Divider = "rgba(255,255,255, 0.12)",
+                DividerLight = "rgba(255,255,255, 0.06)",
+                TableLines = "rgba(255,255,255, 0.12)",
+                LinesDefault = "rgba(255,255,255, 0.12)",
+                LinesInputs = "rgba(255,255,255, 0.3)",
+                TextDisabled = "rgba(255,255,255, 0.2)",
+                Info = "#3299ff",
+                Success = "#0bba83",
+                Warning = "#ffa800",
+                Error = "#f64e62",
+                Dark = "#27272f"
+            }
+        };
 
     public void Dispose()
     {
