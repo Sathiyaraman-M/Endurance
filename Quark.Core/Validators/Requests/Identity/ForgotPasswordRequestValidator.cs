e62d73204f9d1fb3cf5521b77bddfbf9f3bd6ab3
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-26 19:51:40 +0530

    Added fluent validator for ForgotPasswordRequest

---
+++ Quark.Core/Validators/Requests/Identity/ForgotPasswordRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Quark.Core.Validators.Requests.Identity;
+
+public class ForgotPasswordRequestValidator : AbstractValidator<ForgotPasswordRequest>
+{
+    public ForgotPasswordRequestValidator()
+    {
+        RuleFor(request => request.Email)
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => "Email is required")
+                .EmailAddress().WithMessage(x => "Email is not correct");
+    }
+}
