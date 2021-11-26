d94f38ae68734727e3754a5079f229d7572b7156
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-26 20:08:58 +0530

    Added validation rules for date fields

--- Quark.Core/Validators/Features/Checkouts/AddCheckoutCommandValidator.cs
+++ Quark.Core/Validators/Features/Checkouts/AddCheckoutCommandValidator.cs
@@ -8,5 +8,7 @@ public class AddCheckoutCommandValidator : AbstractValidator<AddCheckoutCommand>
     {
         RuleFor(x => x.BookBarcode).NotEmpty().WithMessage(x => "Book Barcode is required");
         RuleFor(x => x.PatronRegisterId).NotEmpty().WithMessage(x => "Patron ID is required");
+        RuleFor(x => x.CheckedOutSince).NotNull().WithMessage(x => "Checkout Date is required");
+        RuleFor(x => x.ExpectedCheckInDate).NotNull().WithMessage(x => "Expected Check In Date is required");
     }
 }
