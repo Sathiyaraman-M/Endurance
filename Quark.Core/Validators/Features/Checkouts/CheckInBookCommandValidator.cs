20af9cf75cb739ee31140ae1c61930c8a518770e
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-26 20:19:38 +0530

    Added fluent validator for CheckInBookCommand

---
+++ Quark.Core/Validators/Features/Checkouts/CheckInBookCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using Quark.Core.Features.Checkouts.Commands;
+
+namespace Quark.Core.Validators.Features.Checkouts;
+
+public class CheckInBookCommandValidator : AbstractValidator<CheckInBookCommand>
+{
+    public CheckInBookCommandValidator()
+    {
+        RuleFor(x => x.BookBarcode).NotEmpty().WithMessage(x => "Book barcode is required");
+        RuleFor(x => x.CheckInDate).NotNull().WithMessage(x => "Check In date is required");
+    }
+}
