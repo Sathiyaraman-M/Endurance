583a149ef0e8c14974d988bc9a3fc194970e40ca
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-11-26 20:00:55 +0530

    Added fluent validator for AddCheckoutCommand

---
+++ Quark.Core/Validators/Features/Checkouts/AddCheckoutCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using Quark.Core.Features.Checkouts.Commands;
+
+namespace Quark.Core.Validators.Features.Checkouts;
+
+public class AddCheckoutCommandValidator : AbstractValidator<AddCheckoutCommand>
+{
+    public AddCheckoutCommandValidator()
+    {
+        RuleFor(x => x.BookBarcode).NotEmpty().WithMessage(x => "Book Barcode is required");
+        RuleFor(x => x.PatronRegisterId).NotEmpty().WithMessage(x => "Patron ID is required");
+    }
+}
