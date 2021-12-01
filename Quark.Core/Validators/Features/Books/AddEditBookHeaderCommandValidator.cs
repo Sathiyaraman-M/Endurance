32276a119b3ebebd373e5e9934e5a4c5b428abee
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-12-01 17:34:19 +0530

    Updated the Fluent Validation based validators

---
+++ Quark.Core/Validators/Features/Books/AddEditBookHeaderCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using Quark.Core.Features.Books.Commands;
+
+namespace Quark.Core.Validators.Features.Books;
+
+public class AddEditBookHeaderCommandValidator : AbstractValidator<AddEditBookHeaderCommand>
+{
+    public AddEditBookHeaderCommandValidator()
+    {
+        RuleFor(x => x.Barcode).NotEmpty().WithMessage(x => "Book barcode cannot be empty");
+    }
+}
