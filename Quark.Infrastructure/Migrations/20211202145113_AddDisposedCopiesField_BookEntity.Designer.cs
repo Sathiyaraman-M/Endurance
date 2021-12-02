9ce62563703c2d725aefb756bf4440b468e49557
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-12-02 23:19:56 +0530

    Added new migration for new field in book entity

--- Quark.Infrastructure/Migrations/20211202145113_AddDisposedCopiesField_BookEntity.Designer.cs
+++ Quark.Infrastructure/Migrations/20211202145113_AddDisposedCopiesField_BookEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Quark.Infrastructure.DbContexts;
 
@@ -11,9 +12,10 @@ using Quark.Infrastructure.DbContexts;
 namespace Quark.Infrastructure.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211202145113_AddDisposedCopiesField_BookEntity")]
+    partial class AddDisposedCopiesField_BookEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Quark.Infrastructure/Migrations/20211202145113_AddDisposedCopiesField_BookEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quark.Infrastructure.Migrations
+{
+    public partial class AddDisposedCopiesField_BookEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisposedCopies",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisposedCopies",
+                table: "Books");
+        }
+    }
+}
