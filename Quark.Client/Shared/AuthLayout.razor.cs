175a8ef293f6282b966cfffbf253cb03f2eb7455
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-12-03 16:12:58 +0530

    Added Separate layout for authentication related pages instead of using MainLayout itself

---
+++ Quark.Client/Shared/AuthLayout.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace Quark.Client.Shared;
+
+public partial class AuthLayout
+{
+    protected override async Task OnInitializedAsync()
+    {
+        _currentTheme = await clientPreferenceManager.GetCurrentThemeAsync();
+        await base.OnInitializedAsync();
+    }
+
+    private MudTheme _currentTheme = new();
+}
