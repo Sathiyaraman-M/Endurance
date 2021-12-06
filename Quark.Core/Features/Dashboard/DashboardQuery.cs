90ad9c140c9fb27066ad71938b7fec90ab14ad94
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-12-06 22:00:24 +0530

    Updated the dashboard query logic to use Guid based IUnitOfWork

--- Quark.Core/Features/Dashboard/DashboardQuery.cs
+++ Quark.Core/Features/Dashboard/DashboardQuery.cs
@@ -7,24 +7,22 @@ public class DashboardQuery : IRequest<Result<DashboardResponse>>
 
 internal class DashboardQueryHandler : IRequestHandler<DashboardQuery, Result<DashboardResponse>>
 {
-    private readonly IUnitOfWork<int> _unitOfWorkInt;
-    private readonly IUnitOfWork<Guid> _unitOfWorkGuid;
+    private readonly IUnitOfWork<Guid> _unitOfWork;
 
-    public DashboardQueryHandler(IUnitOfWork<int> unitOfWorkInt, IUnitOfWork<Guid> unitOfWorkGuid)
+    public DashboardQueryHandler(IUnitOfWork<Guid> unitOfWork)
     {
-        _unitOfWorkInt = unitOfWorkInt;
-        _unitOfWorkGuid = unitOfWorkGuid;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task<Result<DashboardResponse>> Handle(DashboardQuery request, CancellationToken cancellationToken)
     {
         var model = new DashboardResponse
         {
-            PatronsCount = await _unitOfWorkInt.Repository<Patron>().CountAsync(),
-            CheckoutsCount = await _unitOfWorkInt.Repository<Checkout>().CountAsync(),
-            BooksCount = await _unitOfWorkGuid.Repository<Book>().CountAsync(),
-            CheckInTodayCount = await _unitOfWorkInt.Repository<Checkout>().Entities.CountAsync(x => x.CheckedOutUntil.Value.Date == DateTime.Today.Date),
-            CheckInPending = await _unitOfWorkInt.Repository<Checkout>().Entities.CountAsync(x => !x.CheckedOutUntil.HasValue),
+            PatronsCount = await _unitOfWork.Repository<Patron>().CountAsync(),
+            CheckoutsCount = await _unitOfWork.Repository<Checkout>().CountAsync(),
+            BooksCount = await _unitOfWork.Repository<Book>().CountAsync(),
+            CheckInTodayCount = await _unitOfWork.Repository<Checkout>().Entities.CountAsync(x => x.CheckedOutUntil.Value.Date == DateTime.Today.Date, cancellationToken),
+            CheckInPending = await _unitOfWork.Repository<Checkout>().Entities.CountAsync(x => !x.CheckedOutUntil.HasValue, cancellationToken),
         };
         return await Result<DashboardResponse>.SuccessAsync(model);
     }
