5c5f4b6bd00a9f55b1948e0b4de077595b97048e
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-12-08 16:45:45 +0530

    Refactoring code and html design into separate files

---
+++ Quark.Client/Pages/Identity/ForgotPassword.razor.cs
@@ -0,0 +1,30 @@
+﻿namespace Quark.Client.Pages.Identity;
+
+public partial class ForgotPassword
+{
+    private ForgotPasswordRequest requestModel = new();
+
+    private FluentValidationValidator _fluentValidationValidator;
+    private bool Validated => _fluentValidationValidator.Validate(options => options.IncludeAllRuleSets());
+
+    private async Task SubmitAsync()
+    {
+        var response = await userHttpClient.ForgotPasswordAsync(requestModel);
+        if (response.Succeeded)
+        {
+            foreach (var message in response.Messages)
+            {
+                snackbar.Add(message, Severity.Success);
+            }
+            navigationManager.NavigateTo("/");
+        }
+        else
+        {
+            foreach (var message in response.Messages)
+            {
+                snackbar.Add(message, Severity.Error);
+            }
+            requestModel.Email = "";
+        }
+    }
+}
