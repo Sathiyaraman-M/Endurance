ab4345f934c954567b99bea1dfe9cc2900f4977e
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-12-09 17:37:01 +0530

    Added users page and register user dialog

---
+++ Quark.Client/Pages/Identity/RegisterUserModal.razor.cs
@@ -0,0 +1,46 @@
+﻿namespace Quark.Client.Pages.Identity;
+
+public partial class RegisterUserModal
+{
+    private FluentValidationValidator _fluentValidationValidator;
+    private bool Validated => _fluentValidationValidator.Validate(options => { options.IncludeAllRuleSets(); });
+    private readonly RegisterRequest _registerUserModel = new();
+    [CascadingParameter] private MudDialogInstance MudDialog { get; set; }
+
+    private async Task SubmitAsync()
+    {
+        var response = await userHttpClient.RegisterUserAsync(_registerUserModel);
+        if (response.Succeeded)
+        {
+            snackbar.Add(response.Messages[0], Severity.Success);
+            MudDialog.Close();
+        }
+        else
+        {
+            foreach (var message in response.Messages)
+            {
+                snackbar.Add(message, Severity.Error);
+            }
+        }
+    }
+
+    private bool _passwordVisibility;
+    private InputType _passwordInput = InputType.Password;
+    private string _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+    private void TogglePasswordVisibility()
+    {
+        if (_passwordVisibility)
+        {
+            _passwordVisibility = false;
+            _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+            _passwordInput = InputType.Password;
+        }
+        else
+        {
+            _passwordVisibility = true;
+            _passwordInputIcon = Icons.Material.Filled.Visibility;
+            _passwordInput = InputType.Text;
+        }
+    }
+}
--- Quark.Client/Pages/Identity/Users.razor.cs
+++ Quark.Client/Pages/Identity/Users.razor.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Quark.Core.Responses.Identity;
+﻿using Quark.Core.Responses.Identity;
 
 namespace Quark.Client.Pages.Identity;
 
@@ -10,16 +9,16 @@ public partial class Users
     private string _searchString = "";
 
     private ClaimsPrincipal _currentUser;
-    private bool _canCreateUsers;
-    private bool _canExportUsers;
+    private bool _canCreate;
+    private bool _canExport;
     private bool _canViewRoles;
     private bool _loaded;
 
     protected override async Task OnInitializedAsync()
     {
         _currentUser = await authenticationHttpClient.CurrentUser();
-        _canCreateUsers = (await authorizationService.AuthorizeAsync(_currentUser, Permissions.Users.Create)).Succeeded;
-        _canExportUsers = (await authorizationService.AuthorizeAsync(_currentUser, Permissions.Users.Export)).Succeeded;
+        _canCreate = (await authorizationService.AuthorizeAsync(_currentUser, Permissions.Users.Create)).Succeeded;
+        _canExport = (await authorizationService.AuthorizeAsync(_currentUser, Permissions.Users.Export)).Succeeded;
         _canViewRoles = (await authorizationService.AuthorizeAsync(_currentUser, Permissions.Roles.View)).Succeeded;
 
         await GetUsersAsync();
@@ -40,5 +39,64 @@ public partial class Users
                 snackbar.Add(message, Severity.Error);
             }
         }
+    }
+
+    private bool Search(UserResponse user)
+    {
+        if (string.IsNullOrWhiteSpace(_searchString)) return true;
+        if (user.FullName?.Contains(_searchString, StringComparison.OrdinalIgnoreCase) == true)
+        {
+            return true;
+        }
+        if (user.Email?.Contains(_searchString, StringComparison.OrdinalIgnoreCase) == true)
+        {
+            return true;
+        }
+        if (user.PhoneNumber?.Contains(_searchString, StringComparison.OrdinalIgnoreCase) == true)
+        {
+            return true;
+        }
+        if (user.UserName?.Contains(_searchString, StringComparison.OrdinalIgnoreCase) == true)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private async Task ExportToExcel()
+    {
+        var base64 = await userHttpClient.ExportToExcelAsync(_searchString);
+        await jsRuntime.InvokeVoidAsync("Download", new
+        {
+            ByteArray = base64,
+            FileName = $"{nameof(Users).ToLower()}_{DateTime.Now:ddMMyyyyHHmmss}.xlsx",
+            MimeType = ApplicationConstants.MimeTypes.OpenXml
+        });
+        snackbar.Add(string.IsNullOrWhiteSpace(_searchString)
+            ? "Users exported"
+            : "Filtered Users exported", Severity.Success);
+    }
+
+    private async Task InvokeModal()
+    {
+        var parameters = new DialogParameters();
+        var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.Small, FullWidth = true, DisableBackdropClick = true };
+        var dialog = dialogService.Show<RegisterUserModal>("Register New User", parameters, options);
+        var result = await dialog.Result;
+        if (!result.Cancelled)
+        {
+            await GetUsersAsync();
+        }
+    }
+
+    private void ViewProfile(string userId)
+    {
+        navigationManager.NavigateTo($"/user-profile/{userId}");
+    }
+
+    private void ManageRoles(string userId, string email)
+    {
+        if (email == "mukesh@blazorhero.com") snackbar.Add("Not Allowed.", Severity.Error);
+        else navigationManager.NavigateTo($"/identity/user-roles/{userId}");
     }
 }
