092446b962943ea0ef1a32fafac6d4348dfae03d
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-12-15 23:02:27 +0530

    Updated dashboard query

--- Quark.Core/Features/Dashboard/DashboardQuery.cs
+++ Quark.Core/Features/Dashboard/DashboardQuery.cs
@@ -20,8 +20,10 @@ internal class DashboardQueryHandler : IRequestHandler<DashboardQuery, Result<Da
         {
             PatronsCount = await _unitOfWork.Repository<Patron>().CountAsync(),
             CheckoutsCount = await _unitOfWork.Repository<Checkout>().CountAsync(),
-            BooksCount = await _unitOfWork.Repository<Book>().CountAsync(),
+            BooksCount = await _unitOfWork.Repository<Book>().Entities.CountAsync(x => x.BookHeaders.Any(y => y.Condition == AssetStatusConstants.GoodCondition)),
+            BookItemsCount = await _unitOfWork.Repository<BookHeader>().Entities.CountAsync(x => x.Condition == AssetStatusConstants.GoodCondition),
             CheckInTodayCount = await _unitOfWork.Repository<Checkout>().Entities.CountAsync(x => x.CheckedOutUntil.Value.Date == DateTime.Today.Date, cancellationToken),
+            CheckoutTodayCount = await _unitOfWork.Repository<Checkout>().Entities.CountAsync(x => x.CheckedOutSince.Date == DateTime.Today, cancellationToken),
             CheckInPending = await _unitOfWork.Repository<Checkout>().Entities.CountAsync(x => !x.CheckedOutUntil.HasValue, cancellationToken),
         };
         return await Result<DashboardResponse>.SuccessAsync(model);
